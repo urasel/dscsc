f3c2326246781de54a7656389dd410d233dd1799
MD. Rasel <mohammad.rasel@grameensolutions.com>
2018-06-06 16:30:28 +0600

    ddds

--- includes/db_config.php
+++ includes/db_config.php
@@ -5,12 +5,12 @@ define('HOST_NAME', 'localhost');
 define('USER_NAME', 'root');
 define('PASSWORD', '');
 define('DB_PREFIX', 'dscsc_');
-define('DB_NAME', 'dscsc_14032018');
+define('DB_NAME', 'dscsc_6-6-2018');
 
 $Config_host = 'localhost';
 $Config_user = 'root';
 $Config_password = '';
-$Config_db = 'dscsc_14032018';
+$Config_db = 'dscsc_6-6-2018';
 /*
 $dbtablehits= 'hits';
 $dbtableinfo= 'info';*/
